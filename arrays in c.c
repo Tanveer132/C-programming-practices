3d540d393a38ba89ca92356a5a4191d8071bcde7
Tanveer Shikalgar <67576646+Tanveer132@users.noreply.github.com>
2021-06-16 17:54:33 +0530

    Add files via upload

---
+++ arrays in c.c
@@ -0,0 +1,65 @@
+//---------Arrays in C---------
+
+#include<stdio.h>
+
+void display1(int);
+void display2(int*);
+
+int main()
+{
+    /*
+    //array initialization while declaring
+    int arr1[4]={1,2,3,4,5};
+    printf("%d",sizeof(arr1));
+    */
+
+    /*
+    //declare array and enter data and display data
+    int arr2[5];
+
+    for(int i=0;i<=5;i++)
+    {
+        printf("Enter element at arr2[%d]\n",i);
+        scanf("%d",&arr2[i]);
+    }
+
+    for(int i=0;i<=5;i++)
+    {
+        printf("%d ",arr2[i]);
+    }
+    */
+
+    /*
+    //declare array and enter data and pass to function
+    int arr3[5];
+
+    for(int i=0;i<=5;i++)
+    {
+        printf("Enter element at arr3[%d]\n",i);
+        scanf("%d",&arr3[i]);
+    }
+
+    for(int i=0;i<=5;i++)
+    {
+        display1(arr3[i]);
+    }
+    printf("\n");
+    for(int i=0;i<=5;i++)
+    {
+        display2(&arr3[i]);
+    }
+    */
+
+
+    return 0;
+}
+
+void display1(int x)
+{
+    printf("%d ",x);
+}
+
+void display2(int *x)
+{
+    printf("%d ",*x);
+}
